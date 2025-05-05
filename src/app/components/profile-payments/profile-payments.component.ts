d4f58e4feed825e1f336469ede4a1a3641bb215a
Mariam Wahdan <110642313+mariammwahdan@users.noreply.github.com>
2025-05-05 19:48:39 +0300

    user-profile static ui

---
+++ src/app/components/profile-payments/profile-payments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-payments',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-payments.component.html',
+  styleUrl: './profile-payments.component.css'
+})
+export class ProfilePaymentsComponent {
+
+}
